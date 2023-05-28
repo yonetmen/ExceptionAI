39c20991fed58e3cb146127eeabaa456c6e399a4
Kasim Gul <kasimgul@gmail.com>
2023-05-28 02:18:04 +0200

    working

--- src/main/java/com/ksmgl/exceptionai/action/CodeEditorAction.java
+++ src/main/java/com/ksmgl/exceptionai/action/CodeEditorAction.java
@@ -1,6 +1,13 @@
 package com.ksmgl.exceptionai.action;
 
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.markup.HighlighterLayer;
+import com.intellij.openapi.editor.markup.HighlighterTargetArea;
+import com.intellij.openapi.editor.markup.RangeHighlighter;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
@@ -9,35 +16,38 @@ import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.ui.JBColor;
+import kotlin.Pair;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CodeEditorAction {
+  private static final Map<Pair<String, Integer>, RangeHighlighter> highlighters = new HashMap<>();
 
-  public void openClassInCodeEditor(@NotNull Project project, String line) {
+  public void highlightExceptionSourceLine(@NotNull Project project, String line) {
+    clearAllHighlights();
     Pattern pattern = Pattern.compile("at ([a-zA-Z0-9_$.]+)\\.([a-zA-Z0-9_$]+)\\(([A-Za-z0-9-_$.]+\\.java):(\\d+)\\)");
     Matcher sourceClassMatcher = pattern.matcher(line);
     if (PluginToggleAction.isEnabled()) {
       ApplicationManager.getApplication().runReadAction(() -> {
-
         while (sourceClassMatcher.find()) {
           String matchedClassName = sourceClassMatcher.group(1);
-//                String matchedMethodName = matcher1.group(2);
-//                String matchedFileName = matcher1.group(3);
           int matchedLineNumber = Integer.parseInt(sourceClassMatcher.group(4));
 
           // Check if the class belongs to the current project
-          PsiClass psiClass = JavaPsiFacade.getInstance(project)
-              .findClass(matchedClassName, GlobalSearchScope.projectScope(project));
+          PsiClass psiClass = JavaPsiFacade.getInstance(project).findClass(matchedClassName, GlobalSearchScope.projectScope(project));
 
           if (psiClass != null) {
             // Check if the file is in the project's source root
             VirtualFile classFile = psiClass.getContainingFile().getVirtualFile();
             boolean isInSourceRoot = ProjectRootManager.getInstance(project).getFileIndex().isInSourceContent(classFile);
             if (isInSourceRoot) {
-              openClassInEditor(project, psiClass, matchedLineNumber);
+              navigateToAndHighlightLine(project, psiClass, matchedLineNumber);
               break;
             }
           }
@@ -46,7 +56,7 @@ public class CodeEditorAction {
     }
   }
 
-  private void openClassInEditor(Project project, PsiClass psiClass, int lineNumber) {
+  private void navigateToAndHighlightLine(Project project, PsiClass psiClass, int lineNumber) {
     PsiFile psiFile = psiClass.getContainingFile();
     if (psiFile != null) {
       VirtualFile virtualFile = psiFile.getVirtualFile();
@@ -56,9 +66,49 @@ public class CodeEditorAction {
         ApplicationManager.getApplication().invokeLater(() -> {
           if (descriptor.canNavigate()) {
             descriptor.navigate(true);
+            final Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+            if (editor != null) {
+              Runnable[] command = new Runnable[1];
+              command[0] = () -> {
+                if (editor.getCaretModel().getOffset() == descriptor.getOffset()) {
+                  editor.getSelectionModel().selectLineAtCaret();
+                  // Get the document and markup model for the editor
+                  Document document = editor.getDocument();
+
+                  // Get the start and end offsets for the line
+                  int lineStartOffset = document.getLineStartOffset(lineNumber - 1);
+                  int lineEndOffset = document.getLineEndOffset(lineNumber - 1);
+
+                  // Create the text attributes (e.g., with a yellow background)
+                  JBColor semiTransparentYellow = new JBColor(new Color(33, 66, 131, 255), new Color(33, 66, 131, 255));
+                  TextAttributes attributes = new TextAttributes(null, semiTransparentYellow, null, null, 0);
+
+                  Pair<String, Integer> key = new Pair<>(psiClass.getQualifiedName(), lineNumber);
+                  RangeHighlighter existingHighlighter = highlighters.get(key);
+                  if (existingHighlighter == null || !existingHighlighter.isValid()) {
+                    RangeHighlighter highlighter = editor.getMarkupModel().addRangeHighlighter(lineStartOffset, lineEndOffset, HighlighterLayer.ERROR, attributes, HighlighterTargetArea.LINES_IN_RANGE);
+                    highlighters.put(key, highlighter);
+                  }
+                } else {
+                  editor.getCaretModel().moveToOffset(descriptor.getOffset());
+                  ApplicationManager.getApplication().invokeLater(command[0]);
+                }
+              };
+              command[0].run();
+            }
           }
         });
       }
     }
   }
+
+  public void clearAllHighlights() {
+    ApplicationManager.getApplication().invokeLater(() -> {
+      for (Map.Entry<Pair<String, Integer>, RangeHighlighter> entry : highlighters.entrySet()) {
+        entry.getValue().dispose();
+      }
+      highlighters.clear();
+    });
+  }
+
 }
--- src/main/java/com/ksmgl/exceptionai/filter/ExceptionFilter.java
+++ src/main/java/com/ksmgl/exceptionai/filter/ExceptionFilter.java
@@ -7,10 +7,14 @@ import com.ksmgl.exceptionai.action.CodeEditorAction;
 import com.ksmgl.exceptionai.action.ShowSuggestionAction;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExceptionFilter implements ConsoleFilterProvider {
 
   private static final CodeEditorAction cea = new CodeEditorAction();
   private static final ShowSuggestionAction ssa = new ShowSuggestionAction();
+  private static final Pattern pattern = Pattern.compile("at ([a-zA-Z0-9_$.]+)\\.([a-zA-Z0-9_$]+)\\(([A-Za-z0-9-_$.]+\\.java):(\\d+)\\)");
 
   @NotNull
   @Override
@@ -18,8 +22,13 @@ public class ExceptionFilter implements ConsoleFilterProvider {
     return new Filter[]{
         (line, entireLength) -> {
 
-          // Open the Class file in Code Editor
-          cea.openClassInCodeEditor(project, line);
+          Matcher matcher = pattern.matcher(line);
+
+          // Check if line matches the pattern before processing
+          if (matcher.find()) {
+            // Open the Class file in Code Editor
+            cea.highlightExceptionSourceLine(project, line);
+          }
 
           // Show suggestion about the Exception & Error message
           Filter.Result result = ssa.extract(line, entireLength);
